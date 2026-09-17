ac79589b02b89ee67132e8cd98235dba8571b4da
Mint <220407232+MintWinds@users.noreply.github.com>
2026-09-17 08:22:27 -0230

    finish the project

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const myLibraryDisplayed = [];
 
 function Book(title, author, pages, read, id) {
     this.title = title;
@@ -8,13 +9,79 @@ function Book(title, author, pages, read, id) {
     this.id =id;
 
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
     }
 
+}
+
+Book.prototype.hasRead = function(read) {
+    this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read, crypto.randomUUID()))
 
 }
+
+
+function displayLibrary() {
+    while (myLibrary.length > 0) {
+        const book = myLibrary.pop()
+        const bookFrame = document.createElement("div");
+        bookFrame.classList.add("book");
+        let readProperty;
+        for (const property in book) {
+            let isOwn = book.hasOwnProperty(property);
+            if (isOwn && (typeof book[property] !== "function")) {
+                console.log(property);
+                const item = document.createElement("div");
+                item.textContent = book[property]
+                bookFrame.appendChild(item)
+                if (property === "read") {
+                    readProperty = item;
+                }
+            }
+        }
+        const deleteButtonContainer = document.createElement("div");
+        deleteButtonContainer.setAttribute("class", "db-container");
+        const deleteButton = document.createElement("input");
+        deleteButton.setAttribute("type", "button");
+        deleteButton.setAttribute("value", "Delete");
+        const bookcontainer = document.querySelector(".container");
+        bookFrame.appendChild(deleteButtonContainer);
+        deleteButtonContainer.appendChild(deleteButton);
+        bookcontainer.appendChild(bookFrame);
+        deleteButton.addEventListener("click", () => bookcontainer.removeChild(bookFrame))
+
+        const toggleReadContainer = document.createElement("div");
+        toggleReadContainer.setAttribute("class", "db-container");
+        const toggleRead = document.createElement("input");
+        toggleRead.setAttribute("type", "button");
+        toggleRead.setAttribute("value", "Toggle Read");
+        bookFrame.appendChild(toggleReadContainer);
+        toggleReadContainer.appendChild(toggleRead);
+        toggleRead.addEventListener("click", () => {
+            book.hasRead(!(book.read));
+            readProperty.textContent = book.read;
+  
+
+        });
+        
+        
+        myLibraryDisplayed.push(book);
+    }
+    
+}
+
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", () => {
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const read = document.querySelector("#read");
+    addBookToLibrary(title.value, author.value, pages.value, read.value === "yes")
+    displayLibrary();
+})
+
+
 
